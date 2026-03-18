4fc2620e2044ef1dbeea9118589890472f770fa0
Shreyas Rane <shreyasXXXXX@gmail.com>
2026-03-18 23:21:05 +0530

    url added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "/api" });
+const API = axios.create({ baseURL: "https://expensetracker-ck5e.onrender.com//api" });
 
 
 API.interceptors.request.use((req) => {
